7c337a5abb1d87d5bed2a5441714d6fb3dfc533c
Matthias Brettschneider <frosch03@frosch03.de>
2017-08-05 23:17:56 +0200

    added parser for the combined messages returned

--- Protocol.hs
+++ Protocol.hs
@@ -3,12 +3,17 @@
 module Protocol
     ( Name(..)
     , Ready(..)
+    , Messages(..)
     , pickle
     , unpickle
+    , netResult
     )
 where
 
 import Text.JSON.Generic
+import Text.ParserCombinators.Parsec
+
+import Control.Applicative ((<$>))
 
 import qualified Move as M
 import Map 
@@ -25,6 +30,23 @@ data Ready
       { ready :: Int
       } deriving (Show, Data, Typeable)
 
+
+data Messages
+    = Messages [(Int, String)]
+
+instance Read Messages where
+    readsPrec p s = case parse netResult "" s of
+                      Left _  -> error $ "error while parsing Message"
+                      Right x -> [(Messages x, "")]
+      
+
+netResult :: GenParser Char st [(Int, String)]
+netResult
+    = do result <- many message
+         eol
+         return result
+
+
 pickle :: String -> String
 pickle s
     = show l ++ ':':s'
@@ -36,3 +58,43 @@ unpickle :: String -> String
 unpickle = tail . dropWhile (\x -> x /= ':')
 
 
+
+
+data Tree = Leaf String | Node [Tree]
+
+parseTree :: GenParser Char st Tree
+parseTree = node <|> leaf
+  where
+    node = Node <$> between (char '{') (char '}') (many parseTree)
+    leaf = Leaf <$> many1 (noneOf "{}")
+
+nodes :: Tree -> [Tree]
+nodes (Leaf _) = []
+nodes t@(Node ts) = t : concatMap nodes ts
+
+instance Show Tree where
+    showsPrec d (Leaf x) = showString x
+    showsPrec d (Node xs) = showString "{" . showList xs . showString "}"
+        where
+          showList [] = Prelude.id
+          showList (x:xs) = shows x . showList xs
+
+
+message :: GenParser Char st (Int, String)
+message
+    = do len    <- prefix
+         char ':'
+         msgtree <- parseTree
+         return (len, show msgtree)
+    
+remainingMsg :: GenParser Char st String
+remainingMsg
+    = between (char '{') (char '}') (many anyChar)
+
+prefix :: GenParser Char st (Int)
+prefix
+    = do len <- many digit
+         return (read len)
+
+eol :: GenParser Char st Char
+eol = char '\n'
