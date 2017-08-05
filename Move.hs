0914ba3d7a7e2a4d44074789f6400d64cbc03ba0
Matthias Brettschneider <frosch03@frosch03.de>
2017-08-05 12:16:25 +0200

    make move showing more pretty

--- Move.hs
+++ Move.hs
@@ -25,7 +25,18 @@ data Move
       { moves :: [SimpleMove]
       , scores :: [S.Score]
       }
-    deriving (Show, Data, Typeable)
+    deriving (Data, Typeable)
+
+
+instance Show Move where
+    show (Move sms)
+        = "   [" ++ (foldr (\n r -> (show n) ++ ", " ++ r) "]" sms)
+
+    show (Stop sms scs)
+        =    "Game Stopped, Last moves:\n"
+          ++ "   [" ++ (foldr (\n r -> (show n) ++ ", " ++ r) "]\n" sms)
+          ++ "Scores: "
+          ++ "(" ++ (foldr (\n r -> (show n) ++ ", " ++ r) ")" scs)
 
 
 data SimpleMove
@@ -37,5 +48,12 @@ data SimpleMove
     | Pass
       { punter :: PunterId
       }
-    deriving (Show, Data, Typeable)
+    deriving (Data, Typeable)
 
+instance Show SimpleMove where
+    show (Claim p s t)
+        = (show p) ++ ": " ++
+          (show s) ++ "--" ++ (show t)
+
+    show (Pass p)
+        = (show p) ++ ": Pass"
