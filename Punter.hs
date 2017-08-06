20c5edac3077babe93e42e62f3a3fb6b6046bb73
Matthias Brettschneider <frosch03@frosch03.de>
2017-08-06 19:03:27 +0200

    added parser for Settings to harden against these

--- Punter.hs
+++ Punter.hs
@@ -12,10 +12,12 @@ where
 
 import Text.JSON.Generic
 import Control.Monad.State (StateT, liftIO)
+import Text.ParserCombinators.Parsec
 
 import Map 
 import Auxiliary 
-
+import Parsers
+    
 
 type PunterId = Int
 
@@ -34,6 +36,11 @@ data Setup
       , settings :: Settings
       } deriving (Show, Data, Typeable)
 
+instance Read (Setup) where
+    readsPrec p s = case parse pSetup "" s of
+                      Left _  -> error $ "error while parsing Setup"
+                      Right x -> [(x, "")]
+
 data GameState
     = GameState
       { gamemap   :: Map
@@ -44,7 +51,129 @@ data GameState
       , remaining :: Int
       } deriving (Show, Data, Typeable)
 
+
+
+
 type GSM = StateT GameState IO
 
 gsmIO :: IO a -> GSM a
 gsmIO = liftIO
+
+pSetup :: GenParser Char st Setup
+pSetup
+    = do try ( do char '{'
+                  p <- pQuotedInt "punter"
+                  char ','
+                  n <- pQuotedInt "punters"
+                  char ','
+                  string "\"map\":"
+                  char '{'
+                  m  <- pMap
+                  char '}'
+                  char ','
+                  set <- pSettings
+                  char ','
+                  sta <- pState
+                  char '}'
+                  return (Setup p n m sta set)
+             )
+         <|> ( do char '{'
+                  p <- pQuotedInt "punter"
+                  char ','
+                  n <- pQuotedInt "punters"
+                  char ','
+                  string "\"map\":"
+                  char '{'
+                  m  <- pMap
+                  char '}'
+                  char ','
+                  sta <- pState
+                  char '}'
+                  return (Setup p n m sta (Settings False False))
+             )
+
+
+pState :: GenParser Char st GameState
+pState
+    = do string "\"state\":"
+         char '{'
+         char '}'
+         return (GameState (Map [] [] []) 0 0 [] [] 0)
+
+
+
+pSettings :: GenParser Char st Settings
+pSettings
+    = try ( do string "\"settings\":"
+               char '{'
+               sp <- pQuotedBool "splurges"
+               char ','
+               fu <- pQuotedBool "futures"
+               char '}'
+               return (Settings sp fu)
+          )
+      <|>
+      try ( do string "\"settings\":"
+               char '{'
+               fu <- pQuotedBool "futures"
+               char ','
+               sp <- pQuotedBool "splurges"
+               char '}'
+               return (Settings sp fu)
+          )
+      <|>
+      try ( do string "\"settings\":"
+               char '{'
+               sp <- pQuotedBool "splurges"
+               char '}'
+               return (Settings sp False)
+          )
+      <|> ( do string "\"settings\":"
+               char '{'
+               fu <- pQuotedBool "futures"
+               char '}'
+               return (Settings False fu)
+          )
+
+
+pMap :: GenParser Char st Map
+pMap
+    = do sts <- pSites
+         char ','
+         rvs <- pRivers
+         char ','
+         mns <- pQuotedInts "mines"
+         return (Map sts rvs mns)
+
+pRiver :: GenParser Char st River
+pRiver
+    = do char '{'
+         s <- pQuotedInt "source"
+         char ','
+         t  <- pQuotedInt "target"
+         char '}'
+         return (River s t)
+
+pRivers :: GenParser Char st [River]
+pRivers
+    = do string "\"rivers\":"
+         char '['
+         rivers  <- sepBy pRiver (char ',')
+         char ']'
+         return rivers
+
+pSite :: GenParser Char st Site
+pSite
+    = do char '{'
+         s <- pQuotedInt "id"
+         manyTill anyChar $ (try $ char '}')
+         return (Site s)
+
+pSites :: GenParser Char st [Site]
+pSites
+    = do string "\"sites\":"
+         char '['
+         sites  <- sepBy pSite (char ',')
+         char ']'
+         return sites
+
